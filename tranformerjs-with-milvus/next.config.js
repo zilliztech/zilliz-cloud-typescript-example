e99f92f2340fab56489205ec9ef911a0d04376ae
nameczz <zizhao.chen@zilliz.com>
2024-03-25 16:35:17 +0800

    try proto again
    Signed-off-by: nameczz <zizhao.chen@zilliz.com>

--- tranformerjs-with-milvus/next.config.js
+++ tranformerjs-with-milvus/next.config.js
@@ -14,26 +14,26 @@ const nextConfig = {
       "@zilliz/milvus2-sdk-node",
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Copy the proto files to the server build directory
-      config.plugins.push(
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-              from: path.join(
-                __dirname,
-                "node_modules/@zilliz/milvus2-sdk-node/dist"
-              ),
-              to: path.join(__dirname, ".next"),
-            },
-          ],
-        })
-      );
-    }
-    // Important: return the modified config
-    return config;
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     // Copy the proto files to the server build directory
+  //     config.plugins.push(
+  //       new CopyWebpackPlugin({
+  //         patterns: [
+  //           {
+  //             from: path.join(
+  //               __dirname,
+  //               "node_modules/@zilliz/milvus2-sdk-node/dist"
+  //             ),
+  //             to: path.join(__dirname, ".next"),
+  //           },
+  //         ],
+  //       })
+  //     );
+  //   }
+  //   // Important: return the modified config
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
