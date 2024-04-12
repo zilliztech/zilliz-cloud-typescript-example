987565655f333adc996c1ecafbb111bce085899a
nameczz <zizhao.chen@zilliz.com>
2024-04-12 12:25:30 +0800

    fix next config
    Signed-off-by: nameczz <zizhao.chen@zilliz.com>

--- semantic-image-search-client/next.config.js
+++ semantic-image-search-client/next.config.js
@@ -2,19 +2,6 @@
 
 const nextConfig = {
   reactStrictMode: false,
-
-  // Indicate that these packages should not be bundled by webpack
-  experimental: {
-    serverComponentsExternalPackages: [
-      // "sharp",
-      // "onnxruntime-node",
-      "@zilliz/milvus2-sdk-node",
-    ],
-    outputFileTracingIncludes: {
-      // When deploying to Vercel, the following configuration is required
-      "/api/**/*": ["node_modules/@zilliz/milvus2-sdk-node/dist/proto/**/*"],
-    },
-  },
   webpack: (config) => {
     // Ignore node-specific modules when bundling for the browser
     // See https://webpack.js.org/configuration/resolve/#resolvealias
@@ -24,6 +11,14 @@ const nextConfig = {
       "onnxruntime-node$": false,
     };
     return config;
+  },
+  // Indicate that these packages should not be bundled by webpack
+  experimental: {
+    serverComponentsExternalPackages: ["@zilliz/milvus2-sdk-node"],
+    outputFileTracingIncludes: {
+      // When deploying to Vercel, the following configuration is required
+      "/api/**/*": ["node_modules/@zilliz/milvus2-sdk-node/dist/proto/**/*"],
+    },
   },
 };
 
